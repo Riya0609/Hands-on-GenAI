5db34a943b833a8ae5f2ab3ed768046e951282a6
Riya0609 <riyasirohi21@gmail.com>
2025-04-28 12:06:00 +0100

    Update utils.py

--- Multimodal_AI_App/utils.py
+++ Multimodal_AI_App/utils.py
@@ -1,12 +1,13 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import streamlit as st
 
 # Load environment variables once
 load_dotenv()
 
 def get_gemini_client():
-    api_key = os.getenv("GEMINI_API_KEY")
+    api_key = st.secrets.get("GEMINI_API_KEY")
     if not api_key:
         raise ValueError("GEMINI_API_KEY is not set in the .env file.")
     return genai.Client(api_key=api_key)
